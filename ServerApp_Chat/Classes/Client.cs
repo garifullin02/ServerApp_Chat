19951ccb3e7ce5f6f2c07bcd6276b5ebc41afc29
Insaf Garifullin <insafgarifullin02@gmail.com>
2025-03-31 09:29:15 -0700

    add Client class

---
+++ ServerApp_Chat/Classes/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp_Chat.Classes
+{
+    class Client
+    {
+        // _RegistrationCheck = 0
+        // _AuthorizationCheck = 1
+
+        private string _firstName;
+        private string _userName;
+        private string _password;
+        private bool _authorizationAndRegistrationCheck;
+
+        public Client(string firstName, string userName, string password, bool authorizationAndRegistrationCheck)
+        {
+            _firstName = firstName;
+            _userName = userName;
+            _password = password;
+            _authorizationAndRegistrationCheck = authorizationAndRegistrationCheck;
+        }
+    }
+}

558f6dfb2890571a63b6955c899c7fec49cc6b7f
Insaf Garifullin <insafgarifullin02@gmail.com>
2025-04-02 07:46:13 -0700

    create class Group for send data about groups

---
+++ ServerApp_Chat/Classes/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp_Chat.Classes
+{
+    class Group
+    {
+        public string _uid;
+        public string _name;
+        public string _settings;
+        public Client _admin;
+    }
+}
